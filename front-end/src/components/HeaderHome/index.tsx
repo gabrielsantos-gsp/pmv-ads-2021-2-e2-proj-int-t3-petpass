c2263909279dd4aa19210e4b316947835e1ffb52
ArthurBernardoDev <arthurutschgg@gmail.com>
2021-09-22 12:51:21 -0300

    testes

--- front-end/src/components/HeaderHome/index.tsx
+++ front-end/src/components/HeaderHome/index.tsx
@@ -1,53 +1,52 @@
 import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function HeaderHome() {
-    return (
-        <Flex
-            as="header"
-            w="100%"
-            maxWidth={1480}
-            h="20"
-            mx="auto"
-            mt="4"
-            px="6"
-            align="center"
-        // background="#47a9c4"
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+      // background="#47a9c4"
+    >
+      <Text
+        fontSize={["2xl", "3xl"]}
+        fontWeight="bold"
+        letterSpacing="tight"
+        w="64"
+      >
+        PetPass
+      </Text>
+
+      <Flex align="center" ml="auto">
+        <Button
+          type="button"
+          ml="20"
+          fontSize="lg"
+          color="black"
+          _hover={{
+            bg: "#0086ac",
+          }}
         >
-            <Text
-                fontSize={["2xl", "3xl"]}
-                fontWeight="bold"
-                letterSpacing="tight"
-                w="64"
-            >
-                PetPass
-            </Text>
-
-            <Flex
-                align="center"
-                ml="auto">
-                <Button type="button"
-                    ml="20"
-                    fontSize="lg"
-                    color="black"
-                    _hover={{
-                        bg: "#0086ac"
-                    }}
-                >
-                    <Link href="/login">Login</Link>
-                </Button>
-                <Button type="button"
-                    ml="4"
-                    fontSize="lg"
-                    color="black"
-                    _hover={{
-                        bg: "#0086ac"
-                    }}
-                >
-                    <Link href="/registrar">Registrar</Link>
-                </Button>
-            </Flex>
-        </Flex>
-
-
-    );
+          Login
+        </Button>
+        <Button
+          type="button"
+          ml="4"
+          fontSize="lg"
+          color="black"
+          _hover={{
+            bg: "#0086ac",
+          }}
+        >
+          <Link href="/registrar">Registrar</Link>
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
